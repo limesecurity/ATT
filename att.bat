1766a107504fc94276ad27f8e2edef1831a333d6
sonicsketch <sonicsketch@gmail.com>
2018-08-31 14:37:52 +0900

    commit version 1.01 (2018-08-31)

--- att.bat
+++ att.bat
@@ -2,7 +2,7 @@
 SETLOCAL enabledelayedexpansion
 REM //////////////////////////////////////////////////////////////////////////////
 REM ///////////////////// APK Tesing Tool (LIME Security) ////////////////////////
-REM /////////////////////////// 2018-08-26 (VER 1.0) /////////////////////////////
+REM /////////////////////////// VER 1.01 (2018-08-31)) /////////////////////////////
 REM //////////////////////////////////////////////////////////////////////////////
 
 SET current_dir=%cd%
@@ -43,18 +43,19 @@ CLS
 ECHO ------------------------------------------------------------------------------
 ECHO                        APK Testing Tool (Lime Security)
 ECHO ------------------------------------------------------------------------------
-ECHO                            version 1.0 (2018-08-25)
+ECHO                             VER 1.01 (2018-08-31)
 ECHO ------------------------------------------------------------------------------
-ECHO (0. Set Target APK - optional)
 ECHO.
-ECHO 1. Search / Pull APK File
+ECHO 1. Search and Pull APK File with keyword
 ECHO. 
 ECHO 2. Decoding APK                      5. Build APK   
 ECHO 3. Decoding APK (no-res option)      6. Sign APK
 ECHO 4. View Java (With Jad-gui)          7. Install APK
 ECHO.
 ECHO 8. Build + Sign + Install APK
+ECHO.
 ECHO 9. Quit
+ECHO.
 ECHO -------------------------------------------------------------------------------
 SET /p workno=[ATT] SELECT NO: 
 GOTO START
@@ -73,20 +74,11 @@ IF %workno%==10 GOTO HELP
 GOTO END
 
 :SET_TARGET
-IF %mode%==linestyle (
-	IF [%2]==[] (
-		GOTO SELECT_TARGET
-	) ELSE (
-		SET targetapk=%2
-		GOTO GO_WORKING
-	)		
+IF NOT [%2]==[] (
+	SET targetapk=%2
+	GOTO GO_WORKING
 )
-IF %mode%==menustyle (
-	GOTO SELECT_TARGET
-)
-
 :SELECT_TARGET
-ECHO.
 cd "%source_dir%"
 set num=0
 for %%i in (*.apk) do (
@@ -101,7 +93,7 @@ if "%num%"=="0" (
 ECHO.
 SET /p str=[ATT] Select Apk file : 
 IF %str% gtr %num% (
-	SET /p str=[ATT] Cancel and goto Main? [y/n]: 
+	SET /p str=[ATT] Cancel? [y/n]: 
 	IF !str!==y (GOTO END) ELSE (GOTO SELECT_TARGET)
 )
 SET targetapk=!flist[%str%]!
@@ -121,18 +113,14 @@ GOTO END
 
 :PACKAGES
 REM /////////////////////// PACKAGES /////////////////////////////////
-ECHO.
-IF %mode%==linestyle (
-	IF [%2]==[] (
-		SET /p searchkey=[ATT] Input keyword to search packeages : 
-	) ELSE (
-		SET searchkey=%2
-	)
-)
-IF %mode%==menustyle (
+SET CURRENT_WORK=PACAGES_2
+GOTO ADB_CONNECT_TEST
+:PACAGES_2
+IF NOT [%2]==[] (
+	SET searchkey=%2
+) ELSE (
 	SET /p searchkey=[ATT] Input keyword to search packeages : 
 )	
-
 SET num=0
 ECHO [ATT] Searching packages with keyword '%searchkey%' ....
 ECHO.
@@ -150,20 +138,18 @@ for /f "tokens=1,2,3 delims=:=" %%i in ('%ext-tools_dir%\sdk-tools\adb shell pm 
 IF %num%==0 (
 	ECHO [ATT] No packages fonund.
 	GOTO PKG_END
+) ELSE (
+	ECHO.
 )
-ECHO.
 SET /p str=[ATT] Select Apk file to pull : 
 IF %str% gtr %num% (
-	SET /p str=[ATT] Cancel and goto Main? [y/n]: 
+	SET /p str=[ATT] Cancel? [y/n]: 
 	IF !str!==y (GOTO END) ELSE (GOTO PACKAGES)
 )
 SET ori_apk=!pfile[%str%]!
 SET new_apk=!pname[%str%]!.apk
 %ext-tools_dir%\sdk-tools\adb pull %ori_apk% "%source_dir%\%new_apk%"
-ECHO.
-ECHO [ATT] Package saved to :
-ECHO       %source_dir%\%new_apk%
-ECHO         (ref: APK File is saved with package name, Not original apk file name)
+ECHO [ATT] Saved as: %source_dir%\%new_apk%
 
 :PKG_END
 del null
@@ -172,10 +158,8 @@ GOTO END
 
 :DECODE
 REM //////////////////////// DECODE //////////////////////////////////
-REM // CALL로 apktool.bat 를 호출할 경우 -Dfile.encoding=UTF8 세팅으로인해 cmd 창 폰트설정이 리셋되는 문제가 있어 해당 옵션 없이 jar를 직접 실행
-REM // 이 경우, apktool 개발자가 원래 UTF8을 가정하고 작성했을 것이므로 나중에 인코딩에서 오류가 발생하는 경우도 존재할 수 있음
-
-SET str=n
+REM // CALL로 apktool.bat 를 호출할 경우 -Dfile.encoding=UTF8 세팅으로인해 cmd 창 폰트설정이 리셋되는 문제가 있어 해당 옵션을 빼고 jar를 직접 실행
+REM // 개발자가 원래 UTF8을 가정하고 apktool를 작성했을 것이므로 차후 인코딩 오류 이슈가 존재할 수도 있음
 IF exist "%output_dir%\%targetapk%\" (
 	ECHO [ATT] Old directory is exist. : %output_dir%\%targetapk%\
 	SET /p str=[ATT] Overwrite old directory?[y/n] : 
@@ -188,17 +172,13 @@ IF exist "%output_dir%\%targetapk%\" (
 )
 java -jar "%ext-tools_dir%\apktool\apktool.jar" d "%source_dir%"\%targetapk% -o "%output_dir%"\%targetapk%\ -p "%output_dir%"\%targetapk%\framework\
 if errorlevel 1 goto ERROR1
-ECHO.
-ECHO [ATT] Output directory is :
-ECHO       %output_dir%\%targetapk%\
+ECHO [ATT] Output: %output_dir%\%targetapk%\
 :DECODE_END
 GOTO END
 
 
 :DECODE_NO_RES
-REM ///////////////// Decode with no-res option //////////////////////
-
-SET str=n
+REM ///////////////// DECODE with no-res option //////////////////////
 IF exist "%output_dir%\%targetapk%\" (
 	ECHO [ATT] Old directory is exist. : %output_dir%\%targetapk%\
 	SET /p str=[ATT] Overwrite old directory?[y/n] : 
@@ -211,9 +191,7 @@ IF exist "%output_dir%\%targetapk%\" (
 )
 java -jar "%ext-tools_dir%\apktool\apktool.jar" d "%source_dir%"\%targetapk% -r -o "%output_dir%"\%targetapk%\ -p "%output_dir%"\%targetapk%\framework\
 if errorlevel 1 goto ERROR1
-ECHO.
-ECHO [ATT] Output directory is :
-ECHO       %output_dir%\%targetapk%\
+ECHO [ATT] Output: %output_dir%\%targetapk%\
 :DECODE_NO_RES_END
 GOTO END
 
@@ -222,49 +200,41 @@ GOTO END
 REM ///////////////////////// BUILD //////////////////////////////////
 java -jar "%ext-tools_dir%\apktool\apktool.jar" b "%output_dir%"\%targetapk%\ -p "%output_dir%"\%targetapk%\framework\ --force
 if errorlevel 1 goto ERROR1
-ECHO.
-ECHO [ATT] APK file saved to :
-ECHO       %output_dir%\%targetapk%\dist\%targetapk%
+ECHO [ATT] Output: %output_dir%\%targetapk%\dist\%targetapk%
 GOTO END
 
 
 :SIGN
 REM ///////////////////////// SIGN ///////////////////////////////////
-ECHO.
-IF EXIST "%output_dir%\%targetapk%\dist\%targetapk%" (
-	ECHO [ATT] Signing... Please wait.
-	java -jar "%ext-tools_dir%\signapk\signapk.jar" "%ext-tools_dir%\signapk\certificate.pem" "%ext-tools_dir%\signapk\key.pk8" "%output_dir%"\%targetapk%\dist\%targetapk% "%output_dir%"\%targetapk%\dist\signed-%targetapk%
-	if errorlevel 1 goto ERROR1
-	ECHO [ATT] Signed file saved to :
-	ECHO       %output_dir%\%targetapk%\dist\signed-%targetapk%
-	GOTO END
-) ELSE (
-	ECHO.
+IF NOT EXIST "%output_dir%\%targetapk%\dist\%targetapk%" (
 	ECHO [ATT] No apk file.
 	GOTO END
 )
+ECHO [ATT] Signing... Please wait.
+java -jar "%ext-tools_dir%\signapk\signapk.jar" "%ext-tools_dir%\signapk\certificate.pem" "%ext-tools_dir%\signapk\key.pk8" "%output_dir%"\%targetapk%\dist\%targetapk% "%output_dir%"\%targetapk%\dist\signed-%targetapk%
+if errorlevel 1 goto ERROR1
+ECHO [ATT] Output: %output_dir%\%targetapk%\dist\signed-%targetapk%
+GOTO END
 
 
 :INSTALL
 REM //////////////////////// INSTALL /////////////////////////////////
-ECHO.
+SET CURRENT_WORK=INSTALL_2
+GOTO ADB_CONNECT_TEST
+:INSTALL_2
+IF NOT EXIST "%output_dir%"\%targetapk%\dist\signed-%targetapk% (
+	ECHO [ATT] No signed-apk file in build directory. Build and Sign apk file first.
+	GOTO END
+)
 "%ext-tools_dir%\sdk-tools\adb" install -r "%output_dir%"\%targetapk%\dist\signed-%targetapk%
+if errorlevel 1 goto ERROR1
 GOTO END
 
 
 :JAVA_SOURCE
 REM /////////////////////// JAVA_SOURCE //////////////////////////////
-ECHO.
-IF EXIST "%output_dir%\%targetapk%\java\%targetapk%.jar" (
-	"%ext-tools_dir%\jd-gui\jd-gui.exe" "%output_dir%\%targetapk%\java\%targetapk%.jar"
-) ELSE (
-	if exist "%output_dir%"\%targetapk%\build\apk\classes.dex (
-		CALL "%ext-tools_dir%\dex2jar-2.0\d2j-dex2jar.bat" "%output_dir%"\%targetapk%\build\apk\classes.dex --force
-		if errorlevel 1 goto ERROR1
-		mkdir "%output_dir%"\%targetapk%\java"
-		move classes-dex2jar.jar "%output_dir%"\%targetapk%\java\%targetapk%.jar
-		"%ext-tools_dir%\jd-gui\jd-gui.exe" "%output_dir%"\%targetapk%\java\%targetapk%.jar
-	) else (
+IF NOT EXIST "%output_dir%\%targetapk%\java\*.jar" (
+	IF NOT EXIST "%output_dir%"\%targetapk%\build\apk\classes.dex (
 		ECHO [ATT] There is no classes.dex. You must build apk first.
 		SET /p str=[ATT] Do you want build apk?[y/n]: 
 		IF !str!==y (
@@ -275,10 +245,19 @@ IF EXIST "%output_dir%\%targetapk%\java\%targetapk%.jar" (
 			GOTO END
 		)
 	)
+	FOR %%i in ("%output_dir%"\%targetapk%\build\apk\*.dex) DO (
+		CALL "%ext-tools_dir%\dex2jar-2.0\d2j-dex2jar.bat" "%%i" --force
+		if errorlevel 1 goto ERROR1
+	)
+	IF NOT EXIST "%output_dir%"\%targetapk%\java mkdir "%output_dir%"\%targetapk%\java
+	move classes*.jar "%output_dir%"\%targetapk%\java\
+	move classes*.zip "%output_dir%"\%targetapk%\java\
+	ECHO [ATT] Output: "%output_dir%"\%targetapk%\java\%targetapk%.jar
 )
-ECHO.
+"%ext-tools_dir%\jd-gui\jd-gui.exe" "%output_dir%"\%targetapk%\java\*.jar
 ECHO [ATT] Execute jd-gui.exe.
 GOTO END
+
 
 :INSTALL_2
 ECHO.
@@ -293,8 +272,24 @@ ECHO [ATT] Installing %targetapk%...
 GOTO END
 
 
+:ADB_CONNECT_TEST
+adb shell pwd 1>null
+IF errorlevel 1 (
+	ECHO [ATT] Can't connect device.
+) else (
+	GOTO %CURRENT_WORK%
+)
+SET /p str=[ATT] Do you want trying 'adb kill-server'?[y/n]: 
+IF %str%==y (
+	adb kill-server
+	GOTO ADB_CONNECT_TEST
+) else (
+	ECHO [ATT] Manually connect device and try again.
+	GOTO END
+)
+
 :HELP
-ECHO APK file Testing Tool v1.0 (2018-08-25)
+ECHO APK file Testing Tool v1.01 (2018-08-30)
 ECHO [USAGE] ATT.BAT Command Target
 ECHO Command:
 ECHO     p: Search apk file with keyword from connected device. 
@@ -329,6 +324,7 @@ IF %mode%==linestyle GOTO QUIT
 
 
 :QUIT
+IF EXIST null del null
 SET str=
 ECHO.
 
